8c8d47396018063a7bb65f0d195b524db555138a
StarTux <ths.loy@googlemail.com>
2021-08-07 12:28:28 +0200

    Add missing file

---
+++ src/main/java/com/winthier/keepinventory/OptOuts.java
@@ -0,0 +1,9 @@
+package com.winthier.keepinventory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public final class OptOuts {
+    protected Map<UUID, String> players = new HashMap<>();
+}
