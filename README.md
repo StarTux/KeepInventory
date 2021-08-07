cd9c44354892f7944bdc7e3acc3a8b461a1ea7f4
StarTux <ths.loy@googlemail.com>
2021-08-07 12:14:46 +0200

    2021 update
    - Handle exp depending on world difficulty
    - Save via Gson (not Yaml)
    - Tab completion
    - Use Kyori Adventure
    - Explain in README

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # KeepInventory
-Keep your inventory upon death
+
+Keep your inventory upon death. You can opt out of this.
+
+## Commands
+
+- `/keepinventory` View opt-out status
+- `/keepinventory off` Opt out
+- `/keepinventory on` Opt back in (the default)
+
+## Permissions
+
+- `keepinventory.keepinventory` Use the `/keepinventory` command. This
+  permission does not affect inventory keeping.
+
+## Mechanics
+
+If opted out, nothing happens.
+
+If you die while opted in, the all items in your inventory are
+kept. Exp is kept based on world difficulty:
+- **Peaceful** and **Easy**: Keep all exp and levels
+- **Normal**: Lose one level and all exp
+- **Hard**: Vanilla behavior remains unchanged
+
+## Issues
+
+The `keepInventory` game rule must be disabled in your world, or else
+this plugin will ignore you.
+
+## Data storage
+
+Opt-outs are stored in `optouts.json` in the plugin data folder.
--- src/main/java/com/winthier/keepinventory/KeepInventoryPlugin.java
+++ src/main/java/com/winthier/keepinventory/KeepInventoryPlugin.java
@@ -1,169 +1,144 @@
 package com.winthier.keepinventory;
 
+import com.google.gson.Gson;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
-import org.bukkit.ChatColor;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.metadata.LazyMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KeepInventoryPlugin extends JavaPlugin implements Listener {
-    private Set<UUID> optOuts;
-    private boolean supportMiniMap = true;
+    private OptOuts optOuts = null;
+    private Gson gson = new Gson();
 
     @Override
     public void onEnable() {
-        optOuts = null;
-        getDataFolder().mkdirs();
-        getServer().getPluginManager().registerEvents(this, this);
-        for (Player player: getServer().getOnlinePlayers()) {
-            setupPlayer(player);
-        }
+        optOuts = loadOptOuts();
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        if (isOptedOut(event.getEntity())) {
-            return;
-        } else {
-            event.setKeepInventory(true);
-            event.getDrops().clear();
-            return;
-        }
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        setupPlayer(event.getPlayer());
-    }
-
-    private void setupPlayer(Player player) {
-        if (!supportMiniMap) return;
-        final UUID uuid = player.getUniqueId();
-        player.setMetadata("MiniMapSettings", new LazyMetadataValue(this, LazyMetadataValue.CacheStrategy.NEVER_CACHE, () -> {
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("Type", "Boolean");
-                    map.put("Value", !getOptOuts().contains(uuid));
-                    map.put("Priority", 0);
-                    map.put("DisplayName", "Keep Inventory on Death");
-                    Runnable onUpdate = () -> {
-                        final boolean v = map.get("Value") == Boolean.TRUE;
-                        if (!v) {
-                            getOptOuts().add(uuid);
-                        } else {
-                            getOptOuts().remove(uuid);
-                        }
-                        saveOptOuts();
-                    };
-                    map.put("OnUpdate", onUpdate);
-                    List<Map> list = new ArrayList<>();
-                    list.add(map);
-                    return list;
-        }));
-    }
-
+    @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (sender instanceof Player) ? (Player) sender : null;
         if (player == null) {
             sender.sendMessage("Player expected");
             return true;
         }
+        if (args.length > 1) return false;
         if (args.length == 0) {
-            if (isOptedOut(player)) {
-                player.sendMessage((new StringBuilder()).append(ChatColor.RED).append("You lose your inventory when you die.").toString());
-            } else {
-                player.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append("You keep your inventory when you die.").toString());
-            }
-        } else if (args.length == 1) {
-            String arg = args[0].toLowerCase();
-            if (arg.equals("on")) {
-                setOptedOut(player, false);
-                player.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append("From now on, you will keep your inventory when you die.").toString());
-            } else if (arg.equals("off")) {
-                setOptedOut(player, true);
-                player.sendMessage((new StringBuilder()).append(ChatColor.RED).append("From now on, you will lose your inventory when you die.").toString());
-            } else {
-                usage(player);
-                return true;
-            }
-        } else {
+            player.sendMessage(isOptedOut(player)
+                               ? Component.text("You lose your inventory when you die", NamedTextColor.RED)
+                               : Component.text("You keep your inventory when you die", NamedTextColor.GREEN));
+            return true;
+        }
+        switch (args[0]) {
+        case "on":
+            setOptedOut(player, false);
+            player.sendMessage(Component.text("You will keep your inventory when you die", NamedTextColor.GREEN));
+            return true;
+        case "off":
+            setOptedOut(player, true);
+            player.sendMessage(Component.text("You will lose your inventory when you die", NamedTextColor.RED));
+            return true;
+        default:
             usage(player);
             return true;
         }
-        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        return args.length == 1
+            ? Stream.of("on", "off").filter(a -> args[0].contains(a)).collect(Collectors.toList())
+            : Collections.emptyList();
     }
 
     void usage(Player player) {
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&oUsage: &a/KeepInventory &2[On|Off]"));
+        player.sendMessage(Component.text()
+                           .append(Component.text("Usage: ", NamedTextColor.GRAY))
+                           .append(Component.text("/keepinventory ", NamedTextColor.YELLOW))
+                           .append(Component.text("[on|off]", NamedTextColor.GOLD, TextDecoration.ITALIC))
+                           .build());
     }
 
-    File optOutsFile() {
-        return new File(getDataFolder(), "optouts.yml");
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        if (player.getWorld().getGameRuleValue(GameRule.KEEP_INVENTORY)) return;
+        if (isOptedOut(player)) return;
+        event.setKeepInventory(true);
+        event.getDrops().clear();
+        switch (player.getWorld().getDifficulty()) {
+        case PEACEFUL: case EASY:
+            event.setKeepLevel(true);
+            break;
+        case NORMAL:
+            event.setNewLevel(Math.max(0, player.getLevel() - 1));
+            event.setNewExp(0);
+            break;
+        case HARD: default: break;
+        }
     }
 
-    Set<UUID> getOptOuts() {
+    public OptOuts getOptOuts() {
         if (optOuts == null) {
-            optOuts = new HashSet<>();
-            YamlConfiguration config = YamlConfiguration.loadConfiguration(optOutsFile());
-            Iterator iterator = config.getStringList("optouts").iterator();
-            do {
-                if (!iterator.hasNext()) break;
-                String entry = (String) iterator.next();
-                UUID uuid;
-                try {
-                    uuid = UUID.fromString(entry);
-                } catch (IllegalArgumentException iae) {
-                    iae.printStackTrace();
-                    break;
-                }
-                optOuts.add(uuid);
-            } while (true);
+            optOuts = loadOptOuts();
         }
         return optOuts;
     }
 
-    void saveOptOuts() {
-        if (optOuts == null) return;
-        YamlConfiguration config = new YamlConfiguration();
-        List<String> list = new ArrayList<>();
-        UUID uuid;
-        for (Iterator iterator = optOuts.iterator(); iterator.hasNext(); list.add(uuid.toString())) {
-            uuid = (UUID) iterator.next();
-        }
-        config.set("optouts", list);
-        try {
-            config.save(optOutsFile());
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+    public boolean isOptedOut(Player player) {
+        return getOptOuts().players.containsKey(player.getUniqueId());
     }
 
-    boolean isOptedOut(Player player) {
-        return getOptOuts().contains(player.getUniqueId());
-    }
-
-    void setOptedOut(Player player, boolean val) {
+    public void setOptedOut(Player player, boolean val) {
         if (isOptedOut(player) == val) return;
         if (val) {
-            getOptOuts().add(player.getUniqueId());
+            getOptOuts().players.put(player.getUniqueId(), player.getName());
         } else {
-            getOptOuts().remove(player.getUniqueId());
+            getOptOuts().players.remove(player.getUniqueId());
         }
         saveOptOuts();
+    }
+
+    private File getOptOutsFile() {
+        return new File(getDataFolder(), "optouts.json");
+    }
+
+    private OptOuts loadOptOuts() {
+        try (FileReader fr = new FileReader(getOptOutsFile())) {
+            OptOuts result = gson.fromJson(fr, OptOuts.class);
+            return result != null ? result : new OptOuts();
+        } catch (FileNotFoundException fnfr) {
+            return new OptOuts();
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Loading OptOuts", ioe);
+        }
+    }
+
+    private void saveOptOuts() {
+        if (optOuts == null) return;
+        getDataFolder().mkdirs();
+        try (FileWriter fw = new FileWriter(getOptOutsFile())) {
+            gson.toJson(optOuts, fw);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Saving OptOuts", ioe);
+        }
     }
 }
